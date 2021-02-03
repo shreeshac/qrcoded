43ad9fca8888e136ff78c728b7daff2353862f86
shreeshac <shreesha.chidambar@gmail.com>
2021-02-03 13:26:05 +0530

    Update qrcoded.go

--- qrcoded.go
+++ qrcoded.go
@@ -19,6 +19,8 @@ func main() {
 	flag.Parse()
 	http.Handle("/", http.HandlerFunc(rootHandler))
 	http.Handle("/api", http.HandlerFunc(qrCodeGetHandler))
+	http.Handle("/api/file", http.HandlerFunc(qrCodeFileGetHandler))
+	fmt.Println("Listening to port", *addr)
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
@@ -43,6 +45,40 @@ func qrCodeGetHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 }
+
+func qrCodeFileGetHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(405) // Return 405 Method Not Allowed.
+		return
+	}
+	qrtext := req.FormValue("qrtext")
+	qrsize := req.FormValue("qrsize")
+	qrFilePath := req.FormValue("path")
+	if qrsize == "" {
+		qrsize = "128"
+	}
+	if qrFilePath == "" {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "{ \"Message\":\"param `path` is required.\"}")
+
+		return
+	}
+	qrsizeInt, err := strconv.Atoi(qrsize)
+	if err != nil {
+		w.WriteHeader(403)
+		return
+	}
+	if err := qrcode.WriteFile(qrtext, qrcode.Medium, qrsizeInt, qrFilePath); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "{ \"Message\":\"%v.\"}", err)
+		fmt.Println("error", err)
+	}
+
+}
+
 func rootHandler(w http.ResponseWriter, req *http.Request) {
 	templ.Execute(w, req.FormValue("qrtext"))
 
